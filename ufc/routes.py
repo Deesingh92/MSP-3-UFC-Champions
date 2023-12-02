ea76cb71173e4bf946f341fca05a451344421f6b
Dee Torna <tornadavinder@gmail.com>
2023-12-02 19:52:21 +0000

    fighter.html page created also home.html using template inheritance but fighter.html page not currently loading

--- ufc/routes.py
+++ ufc/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from ufc import app, db
 from ufc.models import WeightClass, Champion
 
@@ -6,3 +6,16 @@ from ufc.models import WeightClass, Champion
 @app.route("/")
 def home():
     return render_template("base.html")
+
+@app.route('/fighters', methods=['GET', 'POST'])
+def fighters():
+    if request.method == 'POST':
+        weight_class = request.form.get('weight_class')
+        if weight_class == 'all':
+            champions_filtered = champions
+        else:
+            champions_filtered = [champion for champion in champions if champion['weight_class'] == weight_class]
+
+        return render_template('fighters.html', champions=champions_filtered)
+
+    return render_template('fighters.html', champions=None)
