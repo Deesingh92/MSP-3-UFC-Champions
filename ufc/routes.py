96b98db9734c4703e33626db5b699975240eac16
Dee Torna <tornadavinder@gmail.com>
2023-12-03 19:10:34 +0000

    champion data entered so fighter.html has a search area to find the champions seperated by their weight class

--- ufc/routes.py
+++ ufc/routes.py
@@ -6,7 +6,12 @@ from datetime import datetime
 
 champions = [
     Champion(name="Jon Jones", country="USA", weight_class="Heavyweight", start_date=datetime(2023, 3, 24), end_date=datetime.now()),
-    
+    Champion(name="Alex Pereira", country="Brazil", weight_class="Light Heavyweight", start_date=datetime(2023, 11, 11), end_date=datetime.now()),
+    Champion(name="Sean Strickland", country="USA", weight_class="Middleweight", start_date=datetime(2023, 9, 10), end_date=datetime.now()),
+    Champion(name="Leon Edwards", country="England", weight_class="Welterweight", start_date=datetime(2022, 8, 20), end_date=datetime.now()),
+    Champion(name="Islam Makhachev", country="Dagestan", weight_class="Lightweight", start_date=datetime(2022, 10, 22), end_date=datetime.now()),
+    Champion(name="Alexander Volkanovski", country="Australia", weight_class="Featherweight", start_date=datetime(2019, 12, 14), end_date=datetime.now()),
+    Champion(name="Sean O'Malley", country="USA", weight_class="Bantamweight", start_date=datetime(2022, 8, 20), end_date=datetime.now()),
 ]
 
 @app.route("/")
